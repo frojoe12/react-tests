20ac5539a22cb71291d73db0969b4c66f4ba1993
frojoe12 <45601644+frojoe12@users.noreply.github.com>
2019-10-01 20:23:40 +0100

    Update App.js
    add products to app

--- App.js
+++ App.js
@@ -1,9 +1,32 @@
-import React from "react"
+import React, { Component } from "react"
 
-const App = (props) => (
-    <div className={props.appClass}>
-        {props.text}
-    </div>
-)
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state={
+            products:[]
+        }
+    }
+
+    addProduct = () => {
+        const { products } = this.state
+        const ids = this.state.products.map(product => product.id)
+        const max_id = ids.length > 0 ? Math.max(...ids) : 0
+        products.push({id: max_id+1})
+        this.setState({products})
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Header</h2>
+                {products.map(product => {
+                    <div className="product">{product.title}</div>
+                })}
+                <button className="product-add" onClick={this.addProduct}>New product</button>
+            </div>
+        )
+    }
+}
 
 export default App
