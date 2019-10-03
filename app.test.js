c1490d162c6b9d19eae3a4e51c914228f49f5a9e
frojoe12 <45601644+frojoe12@users.noreply.github.com>
2019-10-03 19:26:18 +0100

    Update app.test.js
    add describe to tests

--- app.test.js
+++ app.test.js
@@ -2,17 +2,32 @@ import React from "react"
 import { shallow } from "enzyme"
 import App from "./App"
 
-const app = shallow(<App />)
+describe("App", ()=> {
 
-it("renders correctly",()=>{
-    expect(app).toMatchSnapshot()
-})
+    const app = shallow(<App />)
 
-it("initialized the `state` with an empty list", ()=> {
-    expect(app.state().products).toEqual([])
-})
+    it("renders correctly",()=>{
+        expect(app).toMatchSnapshot()
+    })
 
-it ('adds a new product to `state` when clicking the `add product` button', ()=> {
-    app.find(".product-add").simulate("click")
-    expect(app.state().product).toEqual([{id:1}])
+    it("initialized the `state` with an empty list", ()=> {
+        expect(app.state().products).toEqual([])
+    })
+
+    describe("when clicking the `add-product` button, () => {
+        
+        beforeEach(()=> {
+            app.find("btn-add").simulate("click")
+        })   
+        afterEach(()=> {
+            app.setState({products:[]})  
+        })
+        it ('adds a new product to `state` when clicking the `add product` button', ()=> {
+            expect(app.state().product).toEqual([{id:1}])
+        })
+    
+        it("adds a new product to the rendered list",()=> {
+           expect(app.find(".product-list").children().length).toEqual(1)  
+        })    
+    })
 })
